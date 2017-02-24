3b9ab50040cb81494ed9faf6e1b14d227e62eac4
felixgouin <felixgouin26@gmail.com>
2017-02-24 15:46:23 +0100

    Create js/script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+        // hover
+        .js document.write('<link href="hover-min.css" rel="stylesheet">');
+        
+        $(function() {
+            setTimeout(function() {
+                $('body').addClass('loaded');
+            }, 0);
+        })
+    
